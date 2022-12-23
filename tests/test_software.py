ccb4ffc5188a7754f094d837a119f2f54ddf2711
whop42 <whop42@icloud.com>
2022-12-23 11:43:18 -0800

    update .gitignore to ignore random tests

--- tests/test_software.py
+++
@@ -1,8 +0,0 @@
-from LinuxConvert.software import Software
-
-def test_software_info(Software s):
-    assert s.name
-    assert s.status == True or s.status = False
-
-if __name__ == "__main__":
-    unittest.main()
