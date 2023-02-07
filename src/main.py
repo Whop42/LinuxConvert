1847fed79a08cfcb9c323bff02b2e680dce489e6
whop42 <whop42@icloud.com>
2023-02-07 15:55:21 -0800

    allow main to work on linux

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import sys, os
-import windows10_setup, linux_setup
 
 if "win" in sys.platform:
+    import windows10_setup
     windows10_setup.main()
 
 if "linux" in sys.platform:
+    import linux_setup
     linux_setup.main(sys.argv[1])
