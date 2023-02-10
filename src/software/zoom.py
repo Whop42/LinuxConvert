4c8f8d6f028079224ad3cfb53f063371be1d81c8
whop42 <whop42@icloud.com>
2023-02-09 17:55:41 -0800

    teams + zoom

---
+++ src/software/zoom.py
@@ -0,0 +1,33 @@
+from software import Software, betterdiscord
+from utils import dprint, eprint, get_root_folder
+import os
+import InfoManager
+
+class zoom(Software.Software):
+    
+    def __init__(self):
+        super(Software.Software, self).__init__()
+        self.name = "zoom"
+
+        self.run_cmd = "/usr/bin/zoom"
+        self.generic_name = "zoom"
+        self.icon = "application-x-zoom"
+    
+    def install(self):
+        return self.install_package("zoom")
+
+    
+    def uninstall(self):
+        return self.remove_package("zoom")
+    
+    def check_install(self):
+        return self.query_package("zoom")
+
+    def check_windows(self):
+        if os.path.isdir(os.path.expandvars("C:\\Program Files\\Zoom")):
+            return True
+        return False
+        
+    def get_config_windows(self):
+        # no config
+        pass
