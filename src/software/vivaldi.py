23eceabf8d6093d6e1d057af8353a3084bd2bfd1
whop42 <whop42@icloud.com>
2023-02-14 18:13:26 -0800

    opera, vivaldi

---
+++ src/software/vivaldi.py
@@ -0,0 +1,36 @@
+from software import Software, betterdiscord
+from utils import dprint, eprint
+import utils
+import os
+import InfoManager
+
+class firefox(Software.Software):
+    
+    def __init__(self):
+        super(Software.Software, self).__init__()
+        self.name = "vivaldi"
+
+        self.run_cmd = "/usr/bin/vivaldi-stable"
+        self.generic_name = "vivaldi"
+        self.icon = "vivaldi"
+    
+    def install(self):
+        out = self.install_packages(["vivaldi", "vivaldi-ffmpeg-codecs"])
+        self.move_configs(os.path.expanduser("~/.config/vivaldi"))
+        return out
+    
+    def uninstall(self):
+        return self.remove_package("vivaldi")
+    
+    def check_install(self):
+        return False
+
+    def check_windows(self):
+        if os.path.isdir(os.path.expandvars("C:\\Users\\$USERNAME\\AppData\\Local\\Vivaldi\\User Data")):
+            return True
+        return False
+    
+    def get_config_windows(self):
+        profiles_path = os.path.expandvars("C:\\Users\\$USERNAME\\AppData\\Local\\Vivaldi\\User Data")
+
+        utils.copy_dir(profiles_path, self.get_config_folder())
