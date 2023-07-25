c06b1db4a7716020c4a2a43c895f486fbcb79fe2
whop42 <whop42@icloud.com>
2023-07-24 18:14:30 -0700

    switch to Package struct for package return

--- linuxconvert/application.go
+++ linuxconvert/application.go
@@ -3,10 +3,15 @@ package linuxconvert
 type Application interface {
 	CheckInstalledWindows() (bool, error)
 	CopyConfigsWindows() error
-	GetPackageLinux() (string, error)
+	GetPackageLinux() (Package, error)
 	InstallConfigsLinux() error
 	GetName() (string, error)
 	SetName(string) error
+}
+
+type Package struct {
+	name           string
+	packageManager string // which package manager to download from: apt, snap, flatpak, etc.
 }
 
 // applications that are available
