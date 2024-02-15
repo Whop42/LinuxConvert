3522a3869b5debcf61b382841e4c0ae86b4b8e7f
whop42 <whop42@icloud.com>
2024-02-14 17:30:32 -0800

    implement CollectApplicationConfigs

--- windows/windows.go
+++ windows/windows.go
@@ -42,8 +42,7 @@ func GenerateWindowsState() *WindowsState {
 GetApplications finds every installed application
 
 output:
-- []app.Application: a slice of installed applications
-- error
+- ws.InstalledApplications updated
 */
 func GetApplications(ws *WindowsState) {
 	//TODO: create test case
@@ -60,10 +59,50 @@ func GetApplications(ws *WindowsState) {
 			util.ErrorLogger.Printf("detecting of %s is installed", a.GetApplicationInformation().Name)
 		}
 	}
+
+	//TODO: unsupported applications system
 }
 
+/*
+CollectApplicationConfigs calls each Application's CopyConfigs() function with the correct dir
+
+output:
+- config files copied, directory structure put in place
+- ws.CollectedApplications updated
+*/
 func CollectApplicationConfigs(ws *WindowsState) {
-	//TODO: implement
+	//TODO: create test case
 
 	// create root directory structure
+	applicationsPath := filepath.Join(ws.StorageDir, "applications")
+	err := os.Mkdir(applicationsPath, os.ModeDir)
+	if err != nil {
+		util.ErrorLogger.Panicf("couldn't create applications folder. error: %s\n", err)
+	}
+
+	// create config.json
+	configJSONPath := filepath.Join(ws.StorageDir, "config.json")
+	configJSON, err := os.Create(configJSONPath)
+	if err != nil {
+		util.ErrorLogger.Panicf("couldn't create config.json. error: %s\n", err)
+	}
+	defer configJSON.Close()
+
+	// copy configs from supported applications
+	for _, app := range ws.InstalledApplications {
+		appDir := filepath.Join(applicationsPath, app.GetApplicationInformation().Name)
+		err = os.Mkdir(appDir, os.ModeDir)
+		if err != nil {
+			util.ErrorLogger.Panicf("couldn't create %s. err: %s\n", appDir, err)
+		}
+
+		err = app.CopyConfigFiles(appDir)
+		if err != nil {
+			util.ErrorLogger.Panicf("couldn't copy config files from %s to %s\n", app.GetApplicationInformation().Name, appDir)
+		}
+
+		ws.CollectedApplications = append(ws.CollectedApplications, app)
+	}
+
+	//TODO: unsupported application system
 }
