bbf8994d7374006b911c9cf9a83441d6461e9524
whop42 <whop42@icloud.com>
2024-02-14 16:26:29 -0800

    create main.go and archive.go (no implementation yet)

---
+++ util/archival.go
@@ -0,0 +1,11 @@
+package util
+
+func Archive(srcDir, dest string) error {
+	//TODO: grab from previous rewrite
+	return nil
+}
+
+func Extract(src, destDir string) error {
+	//TODO: grab from previous rewrite
+	return nil
+}
